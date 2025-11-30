5bd4f6f34fe2af3615b252252bbda922c380d482
webinoo <martin.lucka@gmail.com>
2025-11-30 13:09:20 +0800

    gh-pages

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -10,5 +10,6 @@ export default defineConfig({
     },
     dedupe: ["react", "react-dom"]
   },
-  server: { port: 3000 }
+  server: { port: 3000 },
+  base: '/react-route-profile/',
 });
